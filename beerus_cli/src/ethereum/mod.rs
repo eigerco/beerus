22fc1398f6056f506562e3739b0961784e3ef358
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2022-12-01 09:45:08 +0100

    🚧 fix broken cli and rest api

--- beerus_cli/src/ethereum/mod.rs
+++ beerus_cli/src/ethereum/mod.rs
@@ -13,19 +13,18 @@ pub async fn query_balance(config: Config, address: String) -> Result<()> {
     let mut beerus = BeerusLightClient::new(config)?;
     // Start the Beerus light client.
     beerus.start().await?;
-    //beerus.starknet_state_root().await.unwrap();
     // Parse the Ethereum address.
-    //let addr = Address::from_str(&address)?;
+    let addr = Address::from_str(&address)?;
 
     // TODO: Make the block tag configurable.
-    //let block = BlockTag::Latest;
+    let block = BlockTag::Latest;
     // Query the balance of the Ethereum address.
-    /*let balance = beerus
-    .ethereum_lightclient
-    .get_balance(&addr, block)
-    .await?;*/
+    let balance = beerus
+        .ethereum_lightclient
+        .get_balance(&addr, block)
+        .await?;
     // Format the balance in Ether.
-    /*let balance_in_eth = utils::format_units(balance, "ether")?;
-    println!("{} ETH", balance_in_eth);*/
+    let balance_in_eth = utils::format_units(balance, "ether")?;
+    println!("{} ETH", balance_in_eth);
     Ok(())
 }
