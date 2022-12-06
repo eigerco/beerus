20137fef4dee6a463fbb4a862f4ac776103c6b39
LucasLvy <lucaslevy10@gmail.com>
2022-12-06 18:26:41 +0100

    feat(api): add sn call contract view

--- beerus_rest_api/src/api/starknet/endpoints.rs
+++ beerus_rest_api/src/api/starknet/endpoints.rs
@@ -1,10 +1,14 @@
+use std::str::FromStr;
+
 use crate::api::starknet::resp::QueryStateRootResponse;
 
+use super::resp::QueryContractViewResponse;
 use crate::api::ApiResponse;
 use beerus_core::lightclient::beerus::BeerusLightClient;
 use eyre::Result;
 use log::debug;
 use rocket::{get, State};
+use starknet::core::types::FieldElement;
 
 #[get("/starknet/state/root")]
 pub async fn query_starknet_state_root(
@@ -22,5 +26,49 @@ pub async fn query_starknet_state_root_inner(
     let state_root = beerus.starknet_state_root().await?;
     Ok(QueryStateRootResponse {
         state_root: state_root.to_string(),
+    })
+}
+
+#[get("/starknet/<contract>/<selector>?<calldata>")]
+pub async fn query_starknet_contract_view(
+    beerus: &State<BeerusLightClient>,
+    contract: String,
+    selector: String,
+    calldata: Option<String>,
+) -> ApiResponse<QueryContractViewResponse> {
+    ApiResponse::from_result(
+        query_starknet_contract_view_inner(beerus, contract, selector, calldata).await,
+    )
+}
+
+/// Query a contract view.
+pub async fn query_starknet_contract_view_inner(
+    beerus: &State<BeerusLightClient>,
+    contract_address: String,
+    selector: String,
+    calldata: Option<String>,
+) -> Result<QueryContractViewResponse> {
+    debug!("Querying StarkNet contract view");
+    let contract_address = FieldElement::from_str(&contract_address)?;
+    let selector = FieldElement::from_str(&selector)?;
+    println!("here");
+    let calldata = match calldata {
+        Some(calldata) => calldata
+            .split(',')
+            .map(|s| FieldElement::from_str(s).unwrap())
+            .collect::<Vec<FieldElement>>(),
+
+        None => vec![],
+    };
+    println!("there");
+
+    // Call the StarkNet contract to get the state root.
+    let view = beerus
+        .starknet_call_contract(contract_address, selector, calldata)
+        .await?;
+    println!("over");
+
+    Ok(QueryContractViewResponse {
+        result: view.into_iter().map(|x| x.to_string()).collect(),
     })
 }
