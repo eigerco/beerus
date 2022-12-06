7246c06362dc8a4f7ca135fb66efafdd1c6ae4d4
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2022-12-06 18:51:26 +0100

    🚧 rocket testing

--- beerus_rest_api/src/main.rs
+++ beerus_rest_api/src/main.rs
@@ -1,26 +1,65 @@
-use beerus_rest_api::build_rocket_server;
+use beerus_core::{
+    config::Config,
+    lightclient::{
+        beerus::BeerusLightClient, ethereum::helios_lightclient::HeliosLightClient,
+        starknet::StarkNetLightClientImpl,
+    },
+};
+use beerus_rest_api::api::{ethereum, starknet};
+use log::info;
 use rocket::{Build, Rocket};
 
 #[macro_use]
 extern crate rocket;
 
+#[get("/")]
+fn index() -> &'static str {
+    "Hakai!"
+}
+
 #[launch]
 async fn rocket() -> Rocket<Build> {
-    build_rocket_server().await
+    env_logger::init();
+    info!("starting Beerus Rest API...");
+    // Create config.
+    let config = Config::default();
+
+    // Create a new Ethereum light client.
+    let ethereum_lightclient = HeliosLightClient::new(config.clone()).unwrap();
+    // Create a new StarkNet light client.
+    let starknet_lightclient = StarkNetLightClientImpl::new(&config).unwrap();
+    // Create a new Beerus light client.
+    let mut beerus = BeerusLightClient::new(
+        config,
+        Box::new(ethereum_lightclient),
+        Box::new(starknet_lightclient),
+    );
+
+    // Start the Beerus light client.
+    info!("starting the Beerus light client...");
+    beerus.start().await.unwrap();
+    info!("Beerus light client started and synced.");
+
+    // Create the Rocket instance.
+    rocket::build().manage(beerus).mount(
+        "/",
+        routes![
+            index,
+            ethereum::endpoints::query_balance,
+            starknet::endpoints::query_starknet_state_root
+        ],
+    )
 }
 
 #[cfg(test)]
 mod test {
     use super::rocket;
-    use beerus_rest_api::build_rocket_server;
     use rocket::{http::Status, local::asynchronous::Client};
     /// Test the `query_balance` endpoint.
     /// `/ethereum/balance/<address>`
     #[tokio::test]
-    // For now we ignore this test because it requires to mock the Beerus light client.
-    #[ignore]
     async fn given_normal_conditions_when_query_balance_then_ok() {
-        let client = Client::tracked(build_rocket_server().await)
+        let client = Client::tracked(rocket().await)
             .await
             .expect("valid rocket instance");
         let response = client
