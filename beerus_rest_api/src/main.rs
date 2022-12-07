181a9885c2119065f81e98f4c0a12f4d56f65ecd
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2022-12-07 17:40:02 +0100

    ✅ mock Beerus dependencies for testing of Rest API

--- beerus_rest_api/src/main.rs
+++ beerus_rest_api/src/main.rs
@@ -1,34 +1,34 @@
+use beerus_core::{
+    config::Config,
+    lightclient::{
+        beerus::BeerusLightClient, ethereum::helios_lightclient::HeliosLightClient,
+        starknet::StarkNetLightClientImpl,
+    },
+};
 use beerus_rest_api::build_rocket_server;
 use rocket::{Build, Rocket};
-
 #[macro_use]
 extern crate rocket;
 
 #[launch]
 async fn rocket() -> Rocket<Build> {
-    build_rocket_server().await
-}
+    info!("starting Beerus Rest API...");
+    // Create config.
+    let config = Config::default();
 
-#[cfg(test)]
-mod test {
-    use beerus_rest_api::build_rocket_server;
-    use rocket::{http::Status, local::asynchronous::Client};
-    /// Test the `query_balance` endpoint.
-    /// `/ethereum/balance/<address>`
-    #[tokio::test]
-    // For now we ignore this test because it requires to mock the Beerus light client.
-    #[ignore]
-    async fn given_normal_conditions_when_query_balance_then_ok() {
-        let client = Client::tracked(build_rocket_server().await)
-            .await
-            .expect("valid rocket instance");
-        let response = client
-            .get(uri!(
-                "/ethereum/balance/0xc24215226336d22238a20a72f8e489c005b44c4a"
-            ))
-            .dispatch()
-            .await;
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string().await.unwrap(), "Hello, world!");
-    }
+    // Create a new Ethereum light client.
+    let ethereum_lightclient = HeliosLightClient::new(config.clone()).unwrap();
+    // Create a new StarkNet light client.
+    let starknet_lightclient = StarkNetLightClientImpl::new(&config).unwrap();
+    // Create a new Beerus light client.
+    let mut beerus = BeerusLightClient::new(
+        config,
+        Box::new(ethereum_lightclient),
+        Box::new(starknet_lightclient),
+    );
+    info!("starting the Beerus light client...");
+    beerus.start().await.unwrap();
+    info!("Beerus light client started and synced.");
+
+    build_rocket_server(beerus).await
 }
