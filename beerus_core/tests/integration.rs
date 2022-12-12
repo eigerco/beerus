22b84ae7abc9815c5a96cc0f3808c158851f2f54
LucasLvy <lucaslevy10@gmail.com>
2022-12-12 17:38:44 +0100

    test(beerus_core): integ, mock starknet call

--- beerus_core/tests/integration.rs
+++ beerus_core/tests/integration.rs
@@ -15,7 +15,7 @@ mod test {
     use std::str::FromStr;
 
     #[tokio::test]
-    async fn given_normal_conditions_when_starknet_state_root_should_work() {
+    async fn given_normal_conditions_when_starknet_get_storage_at_should_work() {
         // Start a lightweight mock server.
         let server = MockServer::start();
         let mock_request = get_storage_at_mock(&server);
@@ -69,6 +69,63 @@ mod test {
         assert_eq!(res.unwrap_err().to_string(), expected_error.to_string());
     }
 
+    #[tokio::test]
+    async fn given_normal_conditions_when_starknet_call_should_work() {
+        // Start a lightweight mock server.
+        let server = MockServer::start();
+        let mock_request = call_mock(&server);
+        let config = mock_config(&server);
+
+        let starknet_lightclient = Box::new(StarkNetLightClientImpl::new(&config).unwrap());
+        let mut helios_lightclient = MockEthereumLightClient::new();
+        helios_lightclient
+            .expect_call()
+            .times(1)
+            .return_once(move |_req, _block_nb| Ok(vec![1]));
+        let beerus =
+            BeerusLightClient::new(config, Box::new(helios_lightclient), starknet_lightclient);
+        let storage_var = beerus
+            .starknet_call_contract(
+                FieldElement::from_str("0x00").unwrap(),
+                FieldElement::from_str("0x00").unwrap(),
+                vec![],
+            )
+            .await
+            .unwrap();
+
+        mock_request.assert();
+        assert!(storage_var == vec![FieldElement::from_str("0x01").unwrap()]);
+    }
+
+    #[tokio::test]
+    async fn given_ethereum_light_client_returns_error_when_starknet_starknet_call_should_fail() {
+        let server = MockServer::start();
+        let mock_request = get_storage_at_mock(&server);
+        let config = mock_config(&server);
+
+        let starknet_lightclient = Box::new(StarkNetLightClientImpl::new(&config).unwrap());
+        let mut helios_lightclient = MockEthereumLightClient::new();
+        let expected_error = "Ethereum light client error";
+
+        // Mock the `start` method of the Ethereum light client.
+        helios_lightclient
+            .expect_call()
+            .times(1)
+            .return_once(move |_req, _block_nb| Err(eyre!(expected_error)));
+        let beerus =
+            BeerusLightClient::new(config, Box::new(helios_lightclient), starknet_lightclient);
+        let res = beerus
+            .starknet_call_contract(
+                FieldElement::from_str("0x00").unwrap(),
+                FieldElement::from_str("0x00").unwrap(),
+                vec![],
+            )
+            .await;
+        assert!(mock_request.hits() == 0);
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), expected_error.to_string());
+    }
+
     fn get_storage_at_mock(server: &MockServer) -> Mock {
         server.mock(|when, then| {
             when.method(POST).path("/").json_body(json!({
@@ -89,6 +146,34 @@ mod test {
                     "jsonrpc": "2.0",
                     "id": 1,
                     "result": "0x0000000000000000000000000000000000000000000000000000000000000001"
+                }));
+        })
+    }
+    fn call_mock(server: &MockServer) -> Mock {
+        server.mock(|when, then| {
+            when.method(POST).path("/").json_body(json!({
+                "id":1,
+                "jsonrpc":"2.0",
+                "method":"starknet_call",
+                "params":[
+                    {
+                        "calldata":[
+
+                        ],
+                        "contract_address":"0x0",
+                        "entry_point_selector":"0x0"
+                    },
+                    {
+                        "block_number":1
+                    }
+                ]
+            }));
+            then.status(200)
+                .header("content-type", "application/json")
+                .json_body(json!({
+                    "jsonrpc": "2.0",
+                    "id": 1,
+                    "result": ["0x0000000000000000000000000000000000000000000000000000000000000001"]
                 }));
         })
     }
