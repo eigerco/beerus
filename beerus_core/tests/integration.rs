596804b730032285a411e11a04848f25c2e588f8
LucasLvy <lucaslevy10@gmail.com>
2022-12-12 16:56:05 +0100

    test(beerus_core): integ test,  mock http get_storage_at

---
+++ beerus_core/tests/integration.rs
@@ -0,0 +1,107 @@
+#[cfg(test)]
+mod test {
+    use beerus_core::{
+        config::Config,
+        lightclient::{
+            beerus::BeerusLightClient, ethereum::MockEthereumLightClient,
+            starknet::StarkNetLightClientImpl,
+        },
+    };
+    use ethers::types::Address;
+    use eyre::eyre;
+    use httpmock::{prelude::*, Mock};
+    use serde_json::json;
+    use starknet::core::types::FieldElement;
+    use std::str::FromStr;
+
+    #[tokio::test]
+    async fn given_normal_conditions_when_starknet_state_root_should_work() {
+        // Start a lightweight mock server.
+        let server = MockServer::start();
+        let mock_request = get_storage_at_mock(&server);
+        let config = mock_config(&server);
+
+        let starknet_lightclient = Box::new(StarkNetLightClientImpl::new(&config).unwrap());
+        let mut helios_lightclient = MockEthereumLightClient::new();
+        helios_lightclient
+            .expect_call()
+            .times(1)
+            .return_once(move |_req, _block_nb| Ok(vec![1]));
+        let beerus =
+            BeerusLightClient::new(config, Box::new(helios_lightclient), starknet_lightclient);
+        let storage_var = beerus
+            .starknet_get_storage_at(
+                FieldElement::from_str("0x00").unwrap(),
+                FieldElement::from_str("0x00").unwrap(),
+            )
+            .await
+            .unwrap();
+
+        mock_request.assert();
+        assert!(storage_var == FieldElement::from_str("0x01").unwrap());
+    }
+
+    #[tokio::test]
+    async fn given_ethereum_light_client_returns_error_when_starknet_get_storage_at_should_fail() {
+        let server = MockServer::start();
+        let mock_request = get_storage_at_mock(&server);
+        let config = mock_config(&server);
+
+        let starknet_lightclient = Box::new(StarkNetLightClientImpl::new(&config).unwrap());
+        let mut helios_lightclient = MockEthereumLightClient::new();
+        let expected_error = "Ethereum light client error";
+
+        // Mock the `start` method of the Ethereum light client.
+        helios_lightclient
+            .expect_call()
+            .times(1)
+            .return_once(move |_req, _block_nb| Err(eyre!(expected_error)));
+        let beerus =
+            BeerusLightClient::new(config, Box::new(helios_lightclient), starknet_lightclient);
+        let res = beerus
+            .starknet_get_storage_at(
+                FieldElement::from_str("0x00").unwrap(),
+                FieldElement::from_str("0x00").unwrap(),
+            )
+            .await;
+        assert!(mock_request.hits() == 0);
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), expected_error.to_string());
+    }
+
+    fn get_storage_at_mock(server: &MockServer) -> Mock {
+        server.mock(|when, then| {
+            when.method(POST).path("/").json_body(json!({
+                "id":1,
+                "jsonrpc":"2.0",
+                "method":"starknet_getStorageAt",
+                "params":[
+                    "0x0",
+                    "0x0",
+                    {
+                        "block_number":1
+                    }
+                ]
+            }));
+            then.status(200)
+                .header("content-type", "application/json")
+                .json_body(json!({
+                    "jsonrpc": "2.0",
+                    "id": 1,
+                    "result": "0x0000000000000000000000000000000000000000000000000000000000000001"
+                }));
+        })
+    }
+    fn mock_config(server: &MockServer) -> Config {
+        Config {
+            ethereum_network: "mainnet".to_string(),
+            ethereum_consensus_rpc: server.base_url(),
+            ethereum_execution_rpc: server.base_url(),
+            starknet_rpc: server.base_url(),
+            starknet_core_contract_address: Address::from_str(
+                "0x0000000000000000000000000000000000000000",
+            )
+            .unwrap(),
+        }
+    }
+}
