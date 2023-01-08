d91651b4301586f21971fbeddad5aabaefa36511
danilowhk <danilowhk@gmail.com>
2023-01-08 20:31:15 -0300

    update tests

--- beerus_core/tests/starknet_proofs.rs
+++ beerus_core/tests/starknet_proofs.rs
@@ -32,91 +32,7 @@ fn non_membership() {
 }
 
 #[test]
-fn contract_proof_incorrect_num_elements() {
-    let path = "tests/data/data.json";
-    let s = fs::read_to_string(path).unwrap();
-
-    // Deserialize the JSON file into a GetProofOutput struct
-    let j: JsonOutput = serde_json::from_str(&s).unwrap();
-    let mut output = j.result;
-
-    let contract_address = FieldElement::from_hex_be(
-        "0x4d4e07157aeb54abeb64f5792145f2e8db1c83bda01a8f06e050be18cfb8153",
-    )
-    .unwrap();
-    let state_root = FieldElement::from_hex_be(
-        "0x47f25798a804800b657d4e1508776e3c3c70f0d7587d125a558208f88570aa7",
-    )
-    .unwrap();
-
-    // Modify the contract_proof list to have an incorrect number of elements
-    output.contract_proof = output.contract_proof[..4].to_vec();
-
-    let storage_keys = [FieldElement::ONE];
-    let storage_values = [FieldElement::TWO];
-    let memberships = output.verify(state_root, contract_address, &storage_keys, &storage_values);
-    // The contract_proof list has an incorrect number of elements, this should return None
-    assert_eq!(memberships, None);
-}
-
-#[test]
-fn incorrect_storage_values() {
-    let path = "tests/data/data.json";
-    let s = fs::read_to_string(path).unwrap();
-
-    // Deserialize the JSON file into a GetProofOutput struct
-    let j: JsonOutput = serde_json::from_str(&s).unwrap();
-    let output = j.result;
-
-    let contract_address = FieldElement::from_hex_be(
-        "0x47f25798a804800b657d4e1508776e3c3c70f0d7587d125a558208f88570aa7",
-    )
-    .unwrap();
-    let state_root = FieldElement::from_hex_be(
-        "0x47f25798a804800b657d4e1508776e3c3c70f0d7587d125a558208f88570aa7",
-    )
-    .unwrap();
-
-    // Modify the storage_keys and storage_values arrays to have incorrect values
-    let storage_keys = [FieldElement::ONE];
-    let storage_values = [FieldElement::ZERO];
-    let memberships = output.verify(state_root, contract_address, &storage_keys, &storage_values);
-    // The provided storage keys and values are incorrect, this should return a list of Some(Membership::Member) elements with the correct length, but the storage_proofs field in the ContractData struct is empty
-    assert_eq!(memberships, None);
-}
-
-#[test]
-fn contract_doesnt_exists_correct_storage_keys_values() {
-    let path = "tests/data/data.json";
-    let s = fs::read_to_string(path).unwrap();
-
-    // Deserialize the JSON file into a GetProofOutput struct
-    let j: JsonOutput = serde_json::from_str(&s).unwrap();
-    let output = j.result;
-
-    let contract_address = FieldElement::from_hex_be(
-        "0x4d4e07157aeb54abeb64f5792145f2e8db1c83bda01a8f06e050be18cfb8153",
-    )
-    .unwrap();
-    let state_root = FieldElement::from_hex_be(
-        "0x47f25798a804800b657d4e1508776e3c3c70f0d7587d125a558208f88570aa7",
-    )
-    .unwrap();
-
-    // Use the correct storage_keys and storage_values arrays
-    let storage_keys = [FieldElement::ONE];
-    let storage_values = [FieldElement::TWO];
-    let memberships = output.verify(state_root, contract_address, &storage_keys, &storage_values);
-    // The provided storage keys and values are correct, this should return a list of Some(Membership::NonMember) elements with the correct length, and the storage_proofs field in the ContractData struct is not empty
-    assert_eq!(memberships, Some(vec![Some(Membership::NonMember)]));
-
-    // Check that the storage_proof
-    let contract_data = output.contract_data.unwrap();
-    assert!(!contract_data.storage_proofs.is_empty());
-}
-
-#[test]
-fn invalid_contract_proof() {
+fn invalid_contract_address() {
     // Read in the data.json file
     let path = "tests/data/data.json";
     let s = fs::read_to_string(path).unwrap();
@@ -142,4 +58,57 @@ fn invalid_contract_proof() {
 
     // The verify function should return None, since the contract proof is invalid
     assert_eq!(Option::None, memberships);
+}
+
+#[test]
+fn storage_key_and_storage_values_different_array_lengths() {
+    let path = "tests/data/data.json";
+    let s = fs::read_to_string(path).unwrap();
+
+    // Deserialize the JSON file into a GetProofOutput struct
+    let j: JsonOutput = serde_json::from_str(&s).unwrap();
+    let output = j.result;
+
+    let contract_address = FieldElement::from_hex_be(
+        "0x4d4e07157aeb54abeb64f5792145f2e8db1c83bda01a8f06e050be18cfb8153",
+    )
+    .unwrap();
+    let state_root = FieldElement::from_hex_be(
+        "0x47f25798a804800b657d4e1508776e3c3c70f0d7587d125a558208f88570aa7",
+    )
+    .unwrap();
+
+    // The storage_keys and storage_values arrays are of different lengths, this should return an Err variant
+    let storage_keys = [FieldElement::ONE];
+    let storage_values = [FieldElement::TWO, FieldElement::THREE];
+    let memberships = output.verify(state_root, contract_address, &storage_keys, &storage_values);
+    assert_eq!(Option::None, memberships);
+}
+
+#[test]
+fn contract_proof_incorrect_number_of_elements() {
+    let path = "tests/data/data.json";
+    let s = fs::read_to_string(path).unwrap();
+
+    // Deserialize the JSON file into a GetProofOutput struct
+    let j: JsonOutput = serde_json::from_str(&s).unwrap();
+    let mut output = j.result;
+
+    let contract_address = FieldElement::from_hex_be(
+        "0x4d4e07157aeb54abeb64f5792145f2e8db1c83bda01a8f06e050be18cfb8153",
+    )
+    .unwrap();
+    let state_root = FieldElement::from_hex_be(
+        "0x47f25798a804800b657d4e1508776e3c3c70f0d7587d125a558208f88570aa7",
+    )
+    .unwrap();
+
+    // Modify the contract_proof list to have an incorrect number of elements
+    output.contract_proof = output.contract_proof[..4].to_vec();
+
+    let storage_keys = [FieldElement::ONE];
+    let storage_values = [FieldElement::TWO];
+    let memberships = output.verify(state_root, contract_address, &storage_keys, &storage_values);
+    // The contract_proof list has an incorrect number of elements, this should return None
+    assert_eq!(memberships, None);
 }
