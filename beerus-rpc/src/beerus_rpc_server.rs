1a1cccdd277c1db94af3b4599c3e0be6fa274d15
dpinones <pinonesdamian@gmail.com>
2023-03-09 12:23:26 -0300

    feat: add rpc stark_nonce

--- beerus-rpc/src/beerus_rpc_server.rs
+++ beerus-rpc/src/beerus_rpc_server.rs
@@ -5,6 +5,9 @@ use jsonrpsee::{
     proc_macros::rpc,
 };
 
+use starknet::core::types::FieldElement;
+use std::str::FromStr;
+
 pub struct BeerusRpc {
     _beerus: BeerusLightClient,
 }
@@ -13,12 +16,26 @@ pub struct BeerusRpc {
 trait BeerusApi {
     #[method(name = "hello_world")]
     async fn hello_world(&self) -> Result<String>;
+
+    #[method(name = "stark_nonce")]
+    async fn stark_nonce(&self, contract_address: String) -> Result<String>;
 }
 
 #[async_trait]
 impl BeerusApiServer for BeerusRpc {
     async fn hello_world(&self) -> Result<String> {
         Ok("Hello World!".to_string())
+    }
+
+    async fn stark_nonce(&self, contract_address: String) -> Result<String> {
+        let contract_address = FieldElement::from_str(&contract_address).unwrap();
+        let nonce = self
+            ._beerus
+            .starknet_get_nonce(contract_address)
+            .await
+            .unwrap()
+            .to_string();
+        Ok(nonce)
     }
 }
 
