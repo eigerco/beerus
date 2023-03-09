41a4a18d4f033e76c99596766bc443310f6bd4e3
Paul-Henry Kajfasz <42912740+phklive@users.noreply.github.com>
2023-03-09 11:40:35 -0800

    Feat/stark block number (#237)
    * added feat/stark_blockNumber
    
    * added feat/stark_blockNumber
    
    * Update stark_blockNumber.hurl

--- beerus-rpc/src/beerus_rpc_server.rs
+++ beerus-rpc/src/beerus_rpc_server.rs
@@ -13,12 +13,26 @@ pub struct BeerusRpc {
 trait BeerusApi {
     #[method(name = "hello_world")]
     async fn hello_world(&self) -> Result<String>;
+
+    #[method(name = "stark_blockNumber")]
+    async fn stark_block_number(&self) -> Result<u64>;
 }
 
 #[async_trait]
 impl BeerusApiServer for BeerusRpc {
     async fn hello_world(&self) -> Result<String> {
         Ok("Hello World!".to_string())
+    }
+
+    async fn stark_block_number(&self) -> Result<u64> {
+        let block_number = self
+            ._beerus
+            .starknet_lightclient
+            .block_number()
+            .await
+            .unwrap();
+
+        Ok(block_number)
     }
 }
 
