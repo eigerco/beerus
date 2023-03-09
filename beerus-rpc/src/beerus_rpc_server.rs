cdcd01f7c3c2aaef6aaba3e3cbc4311b5a30ee72
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2023-03-08 18:46:52 -0800

    added feat/stark_chainId

--- beerus-rpc/src/beerus_rpc_server.rs
+++ beerus-rpc/src/beerus_rpc_server.rs
@@ -13,12 +13,27 @@ pub struct BeerusRpc {
 trait BeerusApi {
     #[method(name = "hello_world")]
     async fn hello_world(&self) -> Result<String>;
+
+    #[method(name = "stark_chainId")]
+    async fn stark_chain_id(&self) -> Result<String>;
 }
 
 #[async_trait]
 impl BeerusApiServer for BeerusRpc {
     async fn hello_world(&self) -> Result<String> {
         Ok("Hello World!".to_string())
+    }
+
+    async fn stark_chain_id(&self) -> Result<String> {
+        let chain_id = self
+            ._beerus
+            .starknet_lightclient
+            .chain_id()
+            .await
+            .unwrap()
+            .to_string();
+
+        Ok(chain_id)
     }
 }
 
