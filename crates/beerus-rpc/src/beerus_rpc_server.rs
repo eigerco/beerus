0425675dff08f783b8b9cd532dce1f3ac59d5652
Fred Tupas <fred.tupas21@gmail.com>
2023-03-10 12:44:15 +0100

    feat: add get_class_at RPC endpoint

--- crates/beerus-rpc/src/beerus_rpc_server.rs
+++ crates/beerus-rpc/src/beerus_rpc_server.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use beerus_core::lightclient::beerus::BeerusLightClient;
 /// The RPC module for the Ethereum protocol required by Kakarot.
 use jsonrpsee::{
@@ -6,7 +8,8 @@ use jsonrpsee::{
 };
 
 use beerus_core::starknet_helper::block_id_string_to_block_id_type;
-use starknet::providers::jsonrpc::models::BlockHashAndNumber;
+use starknet::core::types::FieldElement;
+use starknet::providers::jsonrpc::models::{BlockHashAndNumber, ContractClass};
 
 pub struct BeerusRpc {
     _beerus: BeerusLightClient,
@@ -32,6 +35,14 @@ trait BeerusApi {
 
     #[method(name = "stark_blockHashAndNumber")]
     async fn get_block_hash_and_number(&self) -> Result<BlockHashAndNumber>;
+
+    #[method(name = "starknet_getClassAt")]
+    async fn starknet_get_class_at(
+        &self,
+        block_id_type: String,
+        block_id: String,
+        contract_address: String,
+    ) -> Result<ContractClass>;
 }
 
 #[async_trait]
@@ -84,6 +95,22 @@ impl BeerusApiServer for BeerusRpc {
             ._beerus
             .starknet_lightclient
             .block_hash_and_number()
+            .await
+            .unwrap())
+    }
+
+    async fn starknet_get_class_at(
+        &self,
+        block_id_type: String,
+        block_id: String,
+        contract_address: String,
+    ) -> Result<ContractClass> {
+        let block_id = block_id_string_to_block_id_type(&block_id_type, &block_id).unwrap();
+        let contract_address = FieldElement::from_str(&contract_address).unwrap();
+        Ok(self
+            ._beerus
+            .starknet_lightclient
+            .get_class_at(&block_id, contract_address)
             .await
             .unwrap())
     }
