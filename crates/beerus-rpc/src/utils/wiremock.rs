cc3e26984feee085629fa736943738901a1ba17d
Damián Piñones <30808181+dpinones@users.noreply.github.com>
2023-03-13 18:39:48 -0300

    feat: add test starknet_getBlockTransactionCount RPC (#268)
    * test: adding test starknet_getBlockTransactionCount RPC
    
    * Update starknet_getBlockTransactionCount.json

--- crates/beerus-rpc/src/utils/wiremock.rs
+++ crates/beerus-rpc/src/utils/wiremock.rs
@@ -1,5 +1,6 @@
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
+use starknet::providers::jsonrpc::models::{BlockId, BlockTag};
 use wiremock::{
     matchers::{body_json, method},
     Mock, MockServer, ResponseTemplate,
@@ -29,11 +30,21 @@ impl<'a, StarknetParams> StarknetRpcBaseData<'a, StarknetParams> {
             params,
         }
     }
+
+    pub const fn starknet_get_block_transaction_count(params: StarknetParams) -> Self {
+        Self {
+            id: 1,
+            jsonrpc: "2.0",
+            method: "starknet_getBlockTransactionCount",
+            params,
+        }
+    }
 }
 
 pub async fn setup_wiremock() -> String {
     let mock_server = MockServer::start().await;
     mock_block_number().mount(&mock_server).await;
+    mock_get_block_transaction_count().mount(&mock_server).await;
     mock_server.uri()
 }
 
@@ -42,6 +53,18 @@ fn mock_block_number() -> Mock {
         .and(body_json(StarknetRpcBaseData::stark_block_number(())))
         .respond_with(response_template_with_status(StatusCode::OK).set_body_raw(
             include_str!("data/blocks/starknet_blockNumber.json"),
+            "application/json",
+        ))
+}
+
+fn mock_get_block_transaction_count() -> Mock {
+    let latest_block = BlockId::Tag(BlockTag::Latest);
+    Mock::given(method("POST"))
+        .and(body_json(
+            StarknetRpcBaseData::starknet_get_block_transaction_count([&latest_block]),
+        ))
+        .respond_with(response_template_with_status(StatusCode::OK).set_body_raw(
+            include_str!("data/blocks/starknet_getBlockTransactionCount.json"),
             "application/json",
         ))
 }
