60f5e2cfba5e51176baae6783d507a94c485d9fe
dpinones <pinonesdamian@gmail.com>
2023-05-10 00:34:08 -0300

    dev:  CI  and devops

--- book/translate.sh
+++
@@ -1,9 +0,0 @@
-# Refresh the messages.pot, basis for all translations.
-MDBOOK_OUTPUT='{"xgettext": {"pot-file": "messages.pot"}}' \
-mdbook build -d po
-
-# Merge the translations for each languages.
-for po_lang in es fr
-do
-    msgmerge --update po/"$po_lang".po po/messages.pot
-done
