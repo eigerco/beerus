96fc444477596ae7df12bfb216bf31344568927a
Robin Straub <robin@brumaire.io>
2023-05-16 10:56:25 +0200

    ci: add hurl output to summary

--- e2e-tests.sh
+++ e2e-tests.sh
@@ -6,11 +6,75 @@
 # endpoint health status.
 #
 # It will in order:
+# - define a function for computing a hurl summary output
 # - index every .hurl file in the examples/ directory,
 # - execute it and keep track of its success state,
 # - output an inline summary in the console,
 # - generate a summary.md that is reused in the CI job summary,
 # - exit with an error code in case any hurl test fails.
+
+# Prints a markdown report for the given hurl file.
+#
+# @param $1 - the path of the hurl file to execute
+print_hurl_report () {
+  # Compute the group and method associated to the given hurl file
+  group=$(echo "$1" | awk -F'/' '{print $(NF-1)}')
+  method_name=$(echo "$1" | awk -F'/' '{print $NF}' | sed 's/.hurl//')
+
+  # Execute the hurl file and keep track of it result and status
+  result=$(hurl --test --max-time=30 $1 2>&1)
+  status=$?
+
+  # Define the "actual" output
+  actual=$(hurl --ignore-asserts $1 2>&1)
+
+  # Define the status badge
+  if [ $status -eq 0 ]
+  then
+      status_badge="✅"
+  else
+      status_badge="❌"
+  fi
+
+  echo -e "\
+<details>\n
+<summary>$method_name $status_badge</summary>\n\
+\n\
+**Response**\n\
+\`\`\`json\n\
+$actual\n\
+\`\`\`\n\
+\n\
+  "
+
+  if [ $status -ne 0 ]
+  then
+    # note: devnet implementation and [JSON body assertions](https://hurl.dev/docs/asserting-response.html#json-body)
+    # would allow to improve the expectation output by showing the expected body
+    echo -e "\
+**Result**\n\
+\`\`\`json\n\
+$result\n\
+\`\`\`\n\
+\n\
+    "
+  fi
+
+  echo -e "</details>\n"
+}
+
+
+# Generate a markdown summary file
+markdown_file="summary.md"
+
+# Write the summary header
+echo -e "## Hurl Test Execution Summary\n\
+\n\
+This is a summary of the different methods tested.\n\
+\n\
+### Methods\n\
+\n\
+" > "$markdown_file"
 
 # Initialize empty arrays to keep track of succeeding and failing files
 succeeding_files=()
@@ -25,6 +89,9 @@ total_files=$(echo "$all_files" | wc -l | tr -d ' ')
 # Initialize counter
 count=0
 
+# Define a variable to receive the hurl summary outputs
+outputs=""
+
 # Loop over all .hurl files
 while IFS= read -r file
 do
@@ -34,16 +101,32 @@ do
     # Display progress
     echo -e "\n[${count}/${total_files}] Executing: $file"
 
-    # Execute the file with hurl and check the exit status
-    if hurl --test --max-time=30 "$file"
+    # Execute the file with hurl and capture its ouput and exit status
+    output=$(hurl --test --max-time=30 --very-verbose $file 2>&1)
+    status=$?
+
+    # todo: reformat
+    if [ $status -eq 0 ]
     then
         # If the exit status is 0 (success), add file to succeeding_files
         succeeding_files+=("$file")
+        status_badge="✅"
     else
         # If the exit status is non-zero (failure), add file to failing_files
         failing_files+=("$file")
+        status_badge="❌"
     fi
 
+    outputs+=$(echo -e "\
+<details>\n
+<summary>$file $status_badge</summary>\n\
+\n\
+**Response**\n\
+\`\`\`json\n\
+$output\n\
+\`\`\`\n\
+\n\
+</details>") >> "$markdown_file"
 done <<< "$all_files"
 
 # Display failing files
@@ -73,7 +156,7 @@ This is a summary of the different methods tested.\n\
 for method in "${failing_files[@]}"; do
     group=$(echo "$method" | awk -F'/' '{print $(NF-1)}')
     method_name=$(echo "$method" | awk -F'/' '{print $NF}' | sed 's/.hurl//')
-    echo "| $group | $method_name | :x: |" >> "$markdown_file"
+    echo "| $group | $method_name | ❌ |" >> "$markdown_file"
 done
 
 # Add the succeeding methods section
@@ -87,8 +170,14 @@ echo -e "\n\
 for method in "${succeeding_files[@]}"; do
     group=$(echo "$method" | awk -F'/' '{print $(NF-1)}')
     method_name=$(echo "$method" | awk -F'/' '{print $NF}' | sed 's/.hurl//')
-    echo "| $group | $method_name | :heavy_check_mark: |" >> "$markdown_file"
+    echo "| $group | $method_name | ✅ |" >> "$markdown_file"
 done
+
+echo -e "\n\
+### Execution Summary\n\
+\n\
+$outputs\
+" >> "$markdown_file"
 
 # If there are any failing files, return an error
 if [ ${#failing_files[@]} -gt 0 ]
