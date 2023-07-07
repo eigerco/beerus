7f99a7eebc8b63326b9dfb04ecc9afcccf8e3b02
Sumit kumar <106421807+startup-dreamer@users.noreply.github.com>
2023-07-07 19:29:06 +0000

    added send_raw_transaction test

--- crates/beerus-core/tests/helios_lightclient.rs
+++ crates/beerus-core/tests/helios_lightclient.rs
@@ -5,11 +5,16 @@ use common::mock_clients;
 
 mod tests {
     use super::*;
-    use beerus_core::lightclient::ethereum::helios_lightclient::HeliosLightClient;
+    use beerus_core::lightclient::ethereum::{
+        helios_lightclient::HeliosLightClient, EthereumLightClient,
+    };
+    use ethers::types::H256;
+
+    use std::str::FromStr;
 
     /// Test that we can create a Helios light client.
     #[tokio::test]
-    async fn test_helios_lightclient_should_work() {
+    async fn given_normal_conditions_helios_lightclient_should_work() {
         // Mock config.
         let (config, _, _) = mock_clients();
 
@@ -20,14 +25,39 @@ mod tests {
 
     /// Test that we can start a Helios light client.
     #[tokio::test]
-    async fn test_helios_lightclient_should_start() {
-        let (config, _, _) = mock_clients(); // Mock config.
+    async fn given_normal_conditions_helios_lightclient_should_start() {
+        let (config, mut ethereum_lightclient_mock, _) = mock_clients(); // Mock config.
+
+        // Mock the `start` method of the Ethereum light client.
+        ethereum_lightclient_mock
+            .expect_start()
+            .times(1)
+            .return_once(move || Ok(()));
 
         let mut helios_light_client = HeliosLightClient::new(config).await.unwrap();
 
         // Start the Helios light client.
-        let _ = helios_light_client.helios_light_client.start().await;
+        let result = helios_light_client.helios_light_client.start().await;
 
-        assert!(true); // Assert that the test passes.
+        assert!(result.is_ok()); // Assert that the test passes.
+    }
+
+    #[tokio::test]
+    async fn given_normal_conditions_helios_lightclient_should_send_raw_transaction() {
+        let (config, mut ethereum_lightclient_mock, _) = mock_clients(); // Mock config.
+
+        let expected_value =
+            H256::from_str("0xc9bb964b3fe087354bc1c1904518acc2b9df7ebedcb89215e9f3b41f47b6c31d")
+                .unwrap();
+
+        ethereum_lightclient_mock
+            .expect_send_raw_transaction()
+            .return_once(move |_| Ok(expected_value));
+
+        let bytes = &[10];
+
+        let helios_light_client = HeliosLightClient::new(config).await.unwrap();
+        let result = helios_light_client.send_raw_transaction(bytes).await;
+        assert_eq!(expected_value, result.unwrap());
     }
 }
