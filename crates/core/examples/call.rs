0ca4a042743e0a994d91f2f214b372369ac15d9a
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-01-25 22:31:20 +0100

    chore: get rid of some `.unwrap()`s

--- crates/core/examples/call.rs
+++ crates/core/examples/call.rs
@@ -19,15 +19,15 @@ async fn main() -> Result<()> {
         starknet_rpc: format!("https://starknet-mainnet.g.alchemy.com/v2/{api_key}"),
         ..Default::default()
     };
-    let mut beerus = BeerusClient::new(config).await;
+    let mut beerus = BeerusClient::new(config).await?;
     beerus.start().await?;
 
     let block_id = BlockId::Number(33482);
     let starkgate_addr = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
     let name_selector = "0x361458367e696363fbcc70777d07ebbd2394e89fd0adcaf147faccd1d294d60";
     let calldata = FunctionCall {
-        contract_address: FieldElement::from_str(starkgate_addr).unwrap(),
-        entry_point_selector: FieldElement::from_str(name_selector).unwrap(),
+        contract_address: FieldElement::from_str(starkgate_addr)?,
+        entry_point_selector: FieldElement::from_str(name_selector)?,
         calldata: vec![],
     };
 
