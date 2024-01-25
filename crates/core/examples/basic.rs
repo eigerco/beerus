95f6b8baea964eb1ad5a7c258fa4d6b6b01a7347
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-01-25 18:20:25 +0100

    chore: re-organize examples in a cargo-friendly way

---
+++ crates/core/examples/basic.rs
@@ -0,0 +1,26 @@
+use std::env;
+
+use beerus_core::client::BeerusClient;
+use beerus_core::config::Config;
+use eyre::{Context, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let api_key = env::var("ALCHEMY_API_KEY")
+        .context("ALCHEMY_API_KEY is missing")?;
+
+    let config = Config {
+        network: helios::config::networks::Network::MAINNET,
+        eth_execution_rpc: format!("https://eth-mainnet.g.alchemy.com/v2/{api_key}"),
+        starknet_rpc: format!("https://starknet-mainnet.g.alchemy.com/v2/{api_key}"),
+        ..Default::default()
+    };
+    let mut beerus = BeerusClient::new(config).await;
+    beerus.start().await?;
+
+    let current_starknet_block = beerus.sn_state_root().await?;
+    tracing::info!("current starknet block: {current_starknet_block:X}");
+    Ok(())
+}
