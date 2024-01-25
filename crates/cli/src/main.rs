96da0c5b59a3bc7f03ba4dd75d4610e23cfa61ed
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-01-25 22:32:51 +0100

    chore: cleanup clumsy matches on results

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,41 +1,36 @@
 use beerus_core::client::BeerusClient;
 use beerus_core::config::Config;
 use beerus_rpc::BeerusRpc;
+
 use clap::Parser;
-use tracing::{error, info};
+use tracing::info;
 
 #[derive(Parser, Debug)]
 #[command(version)]
 struct Args {
     #[clap(short = 'c', long)]
-    conf: Option<String>,
+    config: Option<String>,
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), String> {
     tracing_subscriber::fmt::init();
 
     let args = Args::parse();
-    let config = match args.conf {
-        Some(conf) => Config::from_file(&conf),
-        None => Config::from_env(),
-    };
+    let config = args.config
+        .map(|config| Config::from_file(&config))
+        .unwrap_or_else(Config::from_env);
 
     info!("init beerus client: {:?}", config.network);
-    let mut beerus = BeerusClient::new(config.clone()).await;
-    if let Err(e) = beerus.start().await {
-        error!("{}", e);
-        std::process::exit(1);
-    };
+    let mut beerus = BeerusClient::new(config).await
+        .map_err(|e| format!("failed to setup beerus client: {e}"))?;
+    beerus.start().await
+        .map_err(|e| format!("failed to start beerus client: {e}"))?;
 
-    match BeerusRpc::new(beerus).run().await {
-        Ok((addr, server_handle)) => {
-            info!("Beerus JSON-RPC server started 🚀: http://{addr}");
-            server_handle.stopped().await;
-        }
-        Err(e) => {
-            error!("{}", e);
-            std::process::exit(1);
-        }
-    };
+    let (address, server) = BeerusRpc::new(beerus).run().await
+        .map_err(|e| format!("failed to start JSON-RPC server: {e}"))?;
+    info!("Beerus JSON-RPC server started 🚀: http://{address}");
+    server.stopped().await;
+
+    Ok(())
 }
