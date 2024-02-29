af246b6b8acf1032a1a13c364bc71740f7d6ff64
psychiller <pscyhiller@protonmail.com>
2024-02-29 20:17:52 +0530

    feat: RPC spec version check on startup

--- crates/rpc/src/lib.rs
+++ crates/rpc/src/lib.rs
@@ -5,8 +5,11 @@ use std::net::SocketAddr;
 
 use api::BeerusRpcServer;
 use beerus_core::client::BeerusClient;
+use error::BeerusRpcError;
 use jsonrpsee::core::Error;
 use jsonrpsee::server::{ServerBuilder, ServerHandle};
+use starknet::providers::Provider;
+use tracing::error;
 
 pub struct BeerusRpc {
     beerus: BeerusClient,
@@ -18,6 +21,27 @@ impl BeerusRpc {
     }
 
     pub async fn run(self) -> Result<(SocketAddr, ServerHandle), Error> {
+        let local_spec_version =
+            self.spec_version().await.map_err(BeerusRpcError::from).unwrap();
+
+        let remote_spec_version = self
+            .beerus
+            .starknet_client
+            .spec_version()
+            .await
+            .map_err(BeerusRpcError::from)
+            .unwrap();
+
+        if local_spec_version != remote_spec_version {
+            error!(
+                "Spec version mismatch between Beerus {} and remote Starknet RPC {}", local_spec_version, remote_spec_version
+            );
+            return Err(Error::Custom(
+                format!("Spec version mismatch between Beerus {} and remote Starkent RPC {}", local_spec_version, remote_spec_version)
+                    .to_string(),
+            ));
+        }
+
         // build the RPC server
         let server =
             ServerBuilder::default().build(self.beerus.config.rpc_addr).await?;
