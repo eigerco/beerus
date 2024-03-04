7d32205e4a8d0505dd7a608bdc942688e456cc65
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-03-04 21:49:04 +0100

    tests(rpc): `blockHashAndNumber`

--- crates/api/tests/rpc.rs
+++ crates/api/tests/rpc.rs
@@ -26,3 +26,16 @@ async fn test_chainId() -> Result<(), common::Error> {
     assert_eq!(ret.as_ref(), "0x534e5f4d41494e");
     Ok(())
 }
+
+#[tokio::test]
+#[allow(non_snake_case)]
+async fn test_blockHashAndNumber() -> Result<(), common::Error> {
+    let Some(ctx) = common::ctx().await else {
+        return Ok(());
+    };
+
+    let ret = ctx.client.blockHashAndNumber().await?;
+    assert!(*ret.block_number.as_ref() > 600612);
+    assert!(!ret.block_hash.0.as_ref().is_empty());
+    Ok(())
+}
