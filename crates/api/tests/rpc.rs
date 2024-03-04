b03fd529237dc1dfcace8d701dd557890acd1ef6
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-03-04 20:37:14 +0100

    fixup: disable example test

--- crates/api/tests/rpc.rs
+++ crates/api/tests/rpc.rs
@@ -1,6 +1,7 @@
 use beerus_api::gen::Rpc;
 
 #[tokio::test]
+#[ignore = "requires a running server"]
 async fn test_rpc() -> Result<(), iamgroot::jsonrpc::Error> {
     let client =
         beerus_api::gen::client::Client::new("http://localhost:9000/rpc");
