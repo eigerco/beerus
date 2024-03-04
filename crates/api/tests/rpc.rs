4caaa1d6e28e1e3213c615dd3c4690bf07167e1c
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-03-04 21:49:56 +0100

    tests(rpc): `estimateFee`

--- crates/api/tests/rpc.rs
+++ crates/api/tests/rpc.rs
@@ -77,3 +77,67 @@ async fn test_call() -> Result<(), common::Error> {
     assert_eq!(ret[0].as_ref(), "0x4574686572");
     Ok(())
 }
+
+#[tokio::test]
+#[allow(non_snake_case)]
+async fn test_estimateFee() -> Result<(), common::Error> {
+    let Some(ctx) = common::ctx().await else {
+        return Ok(());
+    };
+
+    let calldata = vec![
+        "0x2",
+        "0x57c4b510d66eb1188a7173f31cccee47b9736d40185da8144377b896d5ff3",
+        "0x2f0b3c5710379609eb5495f1ecd348cb28167711b73609fe565a72734550354",
+        "0x0",
+        "0x1",
+        "0x57c4b510d66eb1188a7173f31cccee47b9736d40185da8144377b896d5ff3",
+        "0x2f0b3c5710379609eb5495f1ecd348cb28167711b73609fe565a72734550354",
+        "0x1",
+        "0x1",
+        "0x2",
+        "0x0",
+        "0x1",
+    ];
+    let calldata: Result<Vec<Felt>, _> =
+        calldata.into_iter().map(|felt| Felt::try_new(felt)).collect();
+
+    let signature = vec![
+        "0x42527ffe9912b338983cbed67e139cfcc26a4d8cf1d1c2a85e4125fdf5f59ed",
+        "0x636147d06fefd02ed37984b752556d4b9aefdac1a50b3df0528ec7c201ad84b",
+    ];
+    let signature: Result<Vec<Felt>, _> =
+        signature.into_iter().map(|felt| Felt::try_new(felt)).collect();
+
+    let request = vec![
+        BroadcastedTxn::BroadcastedInvokeTxn(
+            BroadcastedInvokeTxn(
+                InvokeTxn::InvokeTxnV1(
+                    InvokeTxnV1 {
+                        calldata: calldata?,
+                        signature: signature?,
+                        sender_address: Address(Felt::try_new("0x13e3ca9a377084c37dc7eacbd1d9f8c3e3733935bcbad887c32a0e213cd6fe0")?), 
+                        max_fee: Felt::try_new("0x28ed6103d0000")?, 
+                        nonce: Felt::try_new("0x1")?,
+                        version: InvokeTxnV1Version::V0x1,
+                        r#type: beerus_api::gen::InvokeTxnV1Type::Invoke, 
+                    }
+                )
+            )
+        )
+    ];
+
+    let simulation_flags = vec![];
+
+    let block_id =
+        BlockId::BlockNumber { block_number: BlockNumber::try_new(59999)? };
+
+    let ret =
+        ctx.client.estimateFee(request, simulation_flags, block_id).await?;
+    assert_eq!(ret.len(), 1);
+    assert_eq!(ret[0].overall_fee.as_ref(), "0x1abd7b153e472");
+    assert_eq!(ret[0].gas_price.as_ref(), "0x67edb4f57");
+    assert_eq!(ret[0].gas_consumed.as_ref(), "0x41de");
+    assert!(matches!(ret[0].unit, PriceUnit::Wei));
+    Ok(())
+}
