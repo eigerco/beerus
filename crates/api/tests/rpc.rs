9dc0346cc908db59b57fa4d823bb99e572ba2bb6
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-03-04 20:37:14 +0100

    style: fmt

--- crates/api/tests/rpc.rs
+++ crates/api/tests/rpc.rs
@@ -2,7 +2,8 @@ use beerus_api::gen::Rpc;
 
 #[tokio::test]
 async fn test_rpc() -> Result<(), iamgroot::jsonrpc::Error> {
-    let client = beerus_api::gen::client::Client::new("http://localhost:9000/rpc");
+    let client =
+        beerus_api::gen::client::Client::new("http://localhost:9000/rpc");
     assert_eq!(client.specVersion().await?, "0.5.1");
     Ok(())
 }
