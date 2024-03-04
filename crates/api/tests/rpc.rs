642def642ca6e60712304b4cfa7c027039f288f8
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-03-04 21:48:39 +0100

    tests(rpc): `chainId`

--- crates/api/tests/rpc.rs
+++ crates/api/tests/rpc.rs
@@ -1,4 +1,7 @@
-use beerus_api::gen::Rpc;
+use beerus_api::gen::{
+    Address, BlockId, BlockNumber, BroadcastedInvokeTxn, BroadcastedTxn, Felt,
+    FunctionCall, InvokeTxn, InvokeTxnV1, InvokeTxnV1Version, PriceUnit, Rpc,
+};
 
 mod common;
 
@@ -8,6 +11,18 @@ async fn test_specVersion() -> Result<(), common::Error> {
     let Some(ctx) = common::ctx().await else {
         return Ok(());
     };
-    assert_eq!(ctx.client.specVersion().await?, "0.6.0");
+    let ret = ctx.client.specVersion().await?;
+    assert_eq!(ret, "0.6.0");
+    Ok(())
+}
+
+#[tokio::test]
+#[allow(non_snake_case)]
+async fn test_chainId() -> Result<(), common::Error> {
+    let Some(ctx) = common::ctx().await else {
+        return Ok(());
+    };
+    let ret = ctx.client.chainId().await?;
+    assert_eq!(ret.as_ref(), "0x534e5f4d41494e");
     Ok(())
 }
