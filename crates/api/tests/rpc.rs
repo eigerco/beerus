b6f7c9147a609623f474235133db9824a07454ab
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-03-04 20:37:14 +0100

    style: fmt

--- crates/api/tests/rpc.rs
+++ crates/api/tests/rpc.rs
@@ -5,7 +5,9 @@ mod common;
 #[tokio::test]
 #[allow(non_snake_case)]
 async fn test_specVersion() -> Result<(), common::Error> {
-    let Some(ctx) = common::ctx().await else { return Ok(()); };
+    let Some(ctx) = common::ctx().await else {
+        return Ok(());
+    };
     assert_eq!(ctx.client.specVersion().await?, "0.6.0");
     Ok(())
 }
