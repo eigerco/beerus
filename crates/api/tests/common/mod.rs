25f9cef1a5536773eb69c88444dc0a11b309a762
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-03-06 12:59:47 +0100

    tests(rpc): narrow test context scope

--- crates/api/tests/common/mod.rs
+++ crates/api/tests/common/mod.rs
@@ -3,8 +3,6 @@ use beerus_api::{
     rpc::{serve, Server},
 };
 
-use async_once_cell::OnceCell;
-
 #[derive(Debug)]
 pub enum Error {
     Rpc(iamgroot::jsonrpc::Error),
@@ -21,21 +19,12 @@ pub struct Context {
     pub server: Server,
 }
 
-async fn setup() -> Option<Context> {
-    tracing_subscriber::fmt::init();
-
-    let url = std::env::var("TEST_URL").ok()?;
+pub async fn ctx() -> Option<Context> {
+    let url = std::env::var("BEERUS_EXPERIMENTAL_TEST_STARKNET_URL").ok()?;
     let server = serve(&url, "127.0.0.1:0").await;
     tracing::info!(port = server.port(), "test server is up");
 
     let url = format!("http://localhost:{}/rpc", server.port());
     let client = Client::new(&url);
     Some(Context { server, client })
-}
-
-// `rstest` does not support async functions as fixtures,
-// thus such "manual" approach with `async_once_cell`
-pub async fn ctx<'a>() -> &'a Option<Context> {
-    static ONCE: OnceCell<Option<Context>> = OnceCell::new();
-    ONCE.get_or_init(async { setup().await }).await
 }
