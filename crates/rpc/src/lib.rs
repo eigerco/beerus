3fad8f767a75b8dc01284707d5e091f4bbcda660
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-03-06 15:32:47 +0100

    feat: add rpc version check

--- crates/rpc/src/lib.rs
+++ crates/rpc/src/lib.rs
@@ -5,8 +5,9 @@ use std::net::SocketAddr;
 
 use api::BeerusRpcServer;
 use beerus_core::client::BeerusClient;
-use jsonrpsee::core::Error;
+use error::BeerusRpcError;
 use jsonrpsee::server::{ServerBuilder, ServerHandle};
+use starknet::providers::Provider;
 
 pub struct BeerusRpc {
     beerus: BeerusClient,
@@ -17,14 +18,28 @@ impl BeerusRpc {
         Self { beerus }
     }
 
-    pub async fn run(self) -> Result<(SocketAddr, ServerHandle), Error> {
-        // build the RPC server
+    pub async fn run(self) -> Result<(SocketAddr, ServerHandle), BeerusRpcError> {
+        self.check_rpc_spec_versions().await?;
+
         let server =
             ServerBuilder::default().build(self.beerus.config.rpc_addr).await?;
 
-        // start the RPC Server
         let addr = server.local_addr()?;
         let handle = server.start(self.into_rpc());
         Ok((addr, handle))
+    }
+
+    async fn check_rpc_spec_versions(&self) -> Result<(), BeerusRpcError> {
+        let expected = self.spec_version().await?;
+
+        let received = self.beerus.starknet_client
+            .spec_version()
+            .await?;
+
+        if expected == received {
+            Ok(())
+        } else {
+            Err(BeerusRpcError::SpecVersionMismatch { expected, received })
+        }
     }
 }
