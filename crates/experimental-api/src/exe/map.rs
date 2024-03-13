3dc9fe1e14759665a95077805d0ad258c597a0bb
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-03-13 22:28:34 +0100

    refactor: extract `build_contract_class` function

--- crates/experimental-api/src/exe/map.rs
+++ crates/experimental-api/src/exe/map.rs
@@ -1,5 +1,7 @@
 use blockifier::execution::contract_class::{ContractClassV0, ContractClassV1};
 
+use self::gen::DeprecatedContractClass;
+
 use super::*;
 
 impl TryFrom<gen::Felt> for StarkFelt {
@@ -38,43 +40,50 @@ impl TryFrom<gen::GetClassResult> for ContractClass {
     type Error = Error;
 
     fn try_from(value: gen::GetClassResult) -> Result<Self, Self::Error> {
-        let json = serde_json::to_string(&value).unwrap();
-        tracing::trace!("Trying to build ContractClass from: {json}");
-
+        let json = serde_json::to_string(&value)?;
         Ok(match value {
             gen::GetClassResult::ContractClass(_) => {
                 let class = ContractClassV1::try_from_json_string(&json)?;
                 ContractClass::V1(class)
             }
             gen::GetClassResult::DeprecatedContractClass(class) => {
-                // TODO: clean this up!
-                let program = {
-                    let program = class.program.as_ref();
-
-                    use base64::{
-                        engine::general_purpose::STANDARD as BASE64,
-                        Engine as _,
-                    };
-                    let program = BASE64.decode(program).unwrap();
-
-                    use flate2::read::GzDecoder;
-                    use std::io::prelude::*;
-                    let mut gz = GzDecoder::<&[u8]>::new(program.as_ref());
-                    let mut result = String::new();
-                    gz.read_to_string(&mut result).unwrap();
-
-                    // println!("result={result}");
-                    result
-                };
-
-                let mut class = serde_json::to_value(class).unwrap();
-                class["program"] = serde_json::from_str(&program).unwrap();
-                let json = serde_json::to_string_pretty(&class).unwrap();
-                // println!("class={json}");
-
-                let class = ContractClassV0::try_from_json_string(&json)?;
+                let class = build_contract_class(class)?;
                 ContractClass::V0(class)
             }
         })
     }
+}
+
+fn build_contract_class(
+    class: DeprecatedContractClass,
+) -> Result<ContractClassV0, Error> {
+    let program = decode_program(class.program.as_ref())?;
+
+    let mut class = serde_json::to_value(class)?;
+    class["program"] = serde_json::from_str(&program)?;
+    let json = serde_json::to_string_pretty(&class)?;
+
+    let class = ContractClassV0::try_from_json_string(&json)?;
+    Ok(class)
+}
+
+fn decode_program(program: &str) -> Result<String, Error> {
+    let program = decode_base64(program)?;
+    let program = decompress(&program)?;
+    Ok(program)
+}
+
+fn decode_base64(input: &str) -> Result<Vec<u8>, Error> {
+    use base64::{engine::general_purpose::STANDARD as BASE64, Engine as _};
+    let result = BASE64.decode(input)?;
+    Ok(result)
+}
+
+fn decompress(input: &[u8]) -> Result<String, Error> {
+    use flate2::read::GzDecoder;
+    use std::io::prelude::*;
+    let mut gz = GzDecoder::new(input);
+    let mut result = String::new();
+    gz.read_to_string(&mut result)?;
+    Ok(result)
 }
