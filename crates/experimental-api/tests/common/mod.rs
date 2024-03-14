a96ba72ab2da11d867b5317676c2bb3a93888329
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-03-14 13:15:10 +0100

    chore: use `[#from]` instead of `impl From` for test errors

--- crates/experimental-api/tests/common/mod.rs
+++ crates/experimental-api/tests/common/mod.rs
@@ -1,51 +1,19 @@
-use beerus_experimental_api::{
-    gen::client::Client,
-    rpc::{serve, Server},
-};
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
+    #[error("rpc call failed: {0:?}")]
     Rpc(iamgroot::jsonrpc::Error),
-    Var(std::env::VarError),
-    Exe(beerus_experimental_api::exe::err::Error),
-    Json(serde_json::Error),
+    #[error("missing env variable: {0:?}")]
+    Var(#[from] std::env::VarError),
+    #[error("execution failed: {0:?}")]
+    Exe(#[from] beerus_experimental_api::exe::err::Error),
+    #[error("serde failed: {0:?}")]
+    Json(#[from] serde_json::Error),
 }
 
 impl From<iamgroot::jsonrpc::Error> for Error {
     fn from(error: iamgroot::jsonrpc::Error) -> Self {
         Self::Rpc(error)
     }
-}
-
-impl From<std::env::VarError> for Error {
-    fn from(error: std::env::VarError) -> Self {
-        Self::Var(error)
-    }
-}
-
-impl From<beerus_experimental_api::exe::err::Error> for Error {
-    fn from(error: beerus_experimental_api::exe::err::Error) -> Self {
-        Self::Exe(error)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(error: serde_json::Error) -> Self {
-        Self::Json(error)
-    }
-}
-
-pub struct Context {
-    pub client: Client,
-    pub server: Server,
-}
-
-pub async fn ctx() -> Option<Context> {
-    let url = std::env::var("BEERUS_EXPERIMENTAL_TEST_STARKNET_URL").ok()?;
-    let server = serve(&url, "127.0.0.1:0").await.ok()?;
-    tracing::info!(port = server.port(), "test server is up");
-
-    let url = format!("http://localhost:{}/rpc", server.port());
-    let client = Client::new(&url);
-    Some(Context { server, client })
 }
