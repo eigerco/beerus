b477acb3c00a1996a2faefd179538a6ee6972b1a
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-05-10 15:52:10 +0200

    chore(deps): bump `iamgroot` dep to 0.2.8

--- crates/experimental-api/tests/common/mod.rs
+++ crates/experimental-api/tests/common/mod.rs
@@ -3,7 +3,7 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("rpc call failed: {0:?}")]
-    Rpc(iamgroot::jsonrpc::Error),
+    Rpc(#[from] iamgroot::jsonrpc::Error),
     #[error("missing env variable: {0:?}")]
     Var(#[from] std::env::VarError),
     #[error("execution failed: {0:?}")]
@@ -12,10 +12,4 @@ pub enum Error {
     Json(#[from] serde_json::Error),
     #[error("starknet api error: {0:?}")]
     Api(#[from] starknet_api::StarknetApiError),
-}
-
-impl From<iamgroot::jsonrpc::Error> for Error {
-    fn from(error: iamgroot::jsonrpc::Error) -> Self {
-        Self::Rpc(error)
-    }
 }
