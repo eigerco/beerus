844f3de7ff0cb06778164b330a14be368dcf3fe7
ICavlek <ivan.cavlek1403@gmail.com>
2024-05-13 13:26:55 +0200

    Introduced checks for Config params

--- crates/core/src/config.rs
+++ crates/core/src/config.rs
@@ -1,6 +1,7 @@
+use std::ffi::OsStr;
 use std::fs;
 use std::net::SocketAddr;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::str::FromStr;
 
 use ethers::types::Address;
@@ -16,6 +17,7 @@ use serde::Deserialize;
 use starknet::core::types::FieldElement;
 use starknet::providers::jsonrpc::{HttpTransport, JsonRpcClient};
 use url::Url;
+use validator::Validate;
 
 // defaults
 pub const DEFAULT_DATA_DIR: &str = "tmp";
@@ -36,15 +38,23 @@ pub const SEPOLIA_CONSENSUS_RPC: &str =
     "http://unstable.sepolia.beacon-api.nimbus.team";
 pub const SEPOLIA_FALLBACK_RPC: &str = "https://sync-sepolia.beaconcha.in";
 
+enum Blockchain {
+    Ethereum,
+    Starknet,
+}
+
 /// global config
-#[derive(Clone, Deserialize, Debug)]
+#[derive(Clone, Deserialize, Debug, Validate)]
 pub struct Config {
     pub network: Network,
+    #[validate(url)]
     pub eth_execution_rpc: String,
+    #[validate(url)]
     pub starknet_rpc: String,
     #[serde(default = "data_dir")]
     pub data_dir: PathBuf,
     #[serde(default = "poll_secs")]
+    #[validate(range(min = 1, max = 10))]
     pub poll_secs: u64,
     #[serde(default = "rpc_addr")]
     pub rpc_addr: SocketAddr,
@@ -107,15 +117,72 @@ impl Config {
 
     pub fn from_file(path: &str) -> Result<Self> {
         let content = fs::read_to_string(path)?;
+        let data_type =
+            match Path::new(path).extension().and_then(OsStr::to_str) {
+                Some(file_extension) => file_extension,
+                None => {
+                    return Err(eyre!(
+                        "No file extension found in provided config file"
+                    ))
+                }
+            };
+        Self::extract_from_file(data_type, content)
+    }
 
-        if path.ends_with(".toml") {
-            let ret: Self = toml::from_str(&content)?;
-            Ok(ret)
-        } else if path.ends_with(".json") {
-            let ret: Self = serde_json::from_str(&content)?;
-            Ok(ret)
-        } else {
-            Err(eyre!("Unsupported config file format"))
+    fn extract_from_file(data_type: &str, content: String) -> Result<Self> {
+        let config_data: Self = match data_type {
+            "toml" => toml::from_str(&content)?,
+            "json" => serde_json::from_str(&content)?,
+            _ => return Err(eyre!("Unsupported config file format")),
+        };
+
+        Ok(config_data)
+    }
+
+    pub async fn validate_params(&self) -> Result<()> {
+        self.validate()?;
+        Self::check_url(&self.eth_execution_rpc, Blockchain::Ethereum).await?;
+        Self::check_url(&self.starknet_rpc, Blockchain::Starknet).await?;
+        self.validate_data_dir()
+    }
+
+    async fn check_url(url: &str, blockchain: Blockchain) -> Result<()> {
+        let name = match blockchain {
+            Blockchain::Ethereum => "eth",
+            Blockchain::Starknet => "starknet",
+        };
+
+        let client = reqwest::Client::new();
+        let response = client
+            .post(url)
+            .header("Content-Type", "application/json")
+            .body(format!(
+                r#"{{"jsonrpc":"2.0","method":"{name}_blockNumber","params":[],"id":0}}"#
+            ))
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        match response.status().is_success() {
+            true => Ok(()),
+            false => Err(eyre!("Wrong response, check {name} url and api key")),
+        }
+    }
+
+    fn validate_data_dir(&self) -> Result<()> {
+        if !self.data_dir.exists() {
+            tracing::warn!("Provided data dir does not exist");
+            return Ok(());
+        };
+
+        let permissions = match self.data_dir.metadata() {
+            Ok(val) => val.permissions(),
+            Err(e) => return Err(e.into()),
+        };
+
+        match permissions.readonly() {
+            false => Ok(()),
+            true => Err(eyre!("Unable to write, read only folder")),
         }
     }
 
@@ -199,5 +266,110 @@ impl Config {
             .await
             .build()
             .expect("incorrect helios client config")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use wiremock::{matchers::any, Mock, MockServer, ResponseTemplate};
+
+    use crate::config::{Blockchain, Config};
+
+    #[tokio::test]
+    async fn wrong_data_type() {
+        let data_type = "yml";
+        let content = "".to_string();
+        assert!(Config::extract_from_file(data_type, content).is_err());
+    }
+
+    #[tokio::test]
+    async fn wrong_content() {
+        let data_type = "toml";
+        let content = "".to_string();
+        assert!(Config::extract_from_file(data_type, content).is_err());
+    }
+
+    #[tokio::test]
+    async fn wrong_urls() {
+        let mut config = Config::default();
+        config.eth_execution_rpc = "foo".to_string();
+        config.starknet_rpc = "bar".to_string();
+
+        let response = config.validate_params().await;
+
+        assert!(response.is_err());
+        assert!(response
+            .unwrap_err()
+            .to_string()
+            .contains("eth_execution_rpc"));
+    }
+
+    #[tokio::test]
+    async fn correct_eth_url() {
+        let eth_mock_server = MockServer::start().await;
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&eth_mock_server)
+            .await;
+
+        assert!(Config::check_url(
+            eth_mock_server.uri().as_str(),
+            Blockchain::Ethereum
+        )
+        .await
+        .is_ok());
+    }
+
+    #[tokio::test]
+    async fn wrong_eth_url() {
+        let eth_mock_server = MockServer::start().await;
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(400))
+            .expect(1)
+            .mount(&eth_mock_server)
+            .await;
+
+        let response = Config::check_url(
+            eth_mock_server.uri().as_str(),
+            Blockchain::Ethereum,
+        )
+        .await;
+
+        assert!(response.is_err());
+        assert!(response.unwrap_err().to_string().contains("eth"));
+    }
+
+    #[tokio::test]
+    async fn wrong_starknet_url() {
+        let starknet_rpc_server = MockServer::start().await;
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(400))
+            .expect(1)
+            .mount(&starknet_rpc_server)
+            .await;
+
+        let response = Config::check_url(
+            starknet_rpc_server.uri().as_str(),
+            Blockchain::Starknet,
+        )
+        .await;
+
+        assert!(response.is_err());
+        assert!(response.unwrap_err().to_string().contains("starknet"));
+    }
+
+    #[tokio::test]
+    async fn wrong_poll_secs() {
+        let mut config = Config::default();
+        config.poll_secs = 99;
+
+        let response = config.validate_params().await;
+
+        assert!(response.is_err());
+        assert!(response.unwrap_err().to_string().contains("poll_secs"));
     }
 }
