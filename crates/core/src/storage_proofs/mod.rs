b35662e20ff276857a4c6f582988085c418ce1cc
Ivan Cavlek <ivan.cavlek1403@gmail.com>
2024-05-31 12:04:39 +0200

    test: introduced tests for StorageProof

--- crates/core/src/storage_proofs/mod.rs
+++ crates/core/src/storage_proofs/mod.rs
@@ -178,3 +178,368 @@ impl StorageProof {
         Some(hold)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use starknet_crypto::FieldElement;
+
+    use crate::{
+        storage_proofs::{
+            types::{ContractData, TrieNode},
+            StorageProof,
+        },
+        utils::felt_to_bits,
+    };
+
+    #[test]
+    fn valid_one_level_parse_proof() {
+        let key = felt_to_bits(
+            FieldElement::from_str(
+                "0x0341c1bdfd89f69748aa00b5742b03adbffd79b8e80cab5c50d91cd8c2a79be1",
+            )
+            .unwrap(),
+        );
+        let value = FieldElement::from_str(
+            "0x000000000000000000000000000047616d65206f66204c69666520546f6b656e",
+        )
+        .unwrap();
+        let edge_node_string = r#"[{
+            "edge": {
+                "child": "0x47616d65206f66204c69666520546f6b656e",
+                "path": {
+                    "len": 231,
+                    "value": "0x3dfd89f69748aa00b5742b03adbffd79b8e80cab5c50d91cd8c2a79be1"
+                }
+            }
+        }]"#;
+        let mut proof: Vec<TrieNode> =
+            serde_json::from_str(edge_node_string).unwrap();
+
+        let value = StorageProof::parse_proof(&key, value, &mut proof).unwrap();
+        assert_eq!(
+            value,
+            FieldElement::from_hex_be(
+                "0x1e224db31dfb3e1b8c95670a12f1903d4a32ac7bb83f4b209029e14155bbca9"
+            )
+            .unwrap()
+        );
+    }
+
+    #[test]
+    fn valid_five_level_parse_proof() {
+        let key = felt_to_bits(
+            FieldElement::from_str(
+                "0x0341c1bdfd89f69748aa00b5742b03adbffd79b8e80cab5c50d91cd8c2a79be1",
+            )
+            .unwrap(),
+        );
+        let value = FieldElement::from_str(
+            "0x000000000000000000000000000047616d65206f66204c69666520546f6b656e",
+        )
+        .unwrap();
+        let proof_string = r#"[
+        {
+            "binary": {
+                "left": "0x46e82293b0564764a071f1aa4488aa7577b1b5bb2e898321f8536d5593d371d",
+                "right": "0x58adcf6ea8b96992aa316e2f092f2480ca406c3630fe97573046a32900745b5"
+            }
+        },
+        {
+            "binary": {
+                "left": "0x716e211c75f4c0e14dbe46c361812b0129abd061b63faf91ad5569bf22b785c",
+                "right": "0x3729d9699d4410223e413f3b3aa91a043d94242f888188036e6ea25b6962041"
+            }
+        },
+        {
+            "edge": {
+                "child": "0x6281e42b5941ae1a77ea03836aad1190097f72e1a1ed534fae2e00b4118f504",
+                "path": {
+                    "len": 1,
+                    "value": "0x1"
+                }
+            }
+        },
+        {
+            "binary": {
+                "left": "0x3e3800516f62800ef6491b1cb1915b3353026ea6a6afcf35e8d4c54e35b04ea",
+                "right": "0x1e224db31dfb3e1b8c95670a12f1903d4a32ac7bb83f4b209029e14155bbca9"
+            }
+        },
+        {
+            "edge": {
+                "child": "0x47616d65206f66204c69666520546f6b656e",
+                "path": {
+                    "len": 231,
+                    "value": "0x3dfd89f69748aa00b5742b03adbffd79b8e80cab5c50d91cd8c2a79be1"
+                }
+            }
+        }]"#;
+        let mut proof: Vec<TrieNode> =
+            serde_json::from_str(proof_string).unwrap();
+
+        let value = StorageProof::parse_proof(&key, value, &mut proof).unwrap();
+        assert_eq!(
+            value,
+            FieldElement::from_hex_be(
+                "0x6cc50a732b4256f7b642348e19bd1a8bee7ac76bed3fcee3bc34309538c00c6"
+            )
+            .unwrap()
+        );
+    }
+
+    #[test]
+    fn invalid_one_level_parse_proof() {
+        let key = felt_to_bits(FieldElement::default());
+        let value = FieldElement::ZERO;
+        let mut proof: Vec<TrieNode> = serde_json::from_str(
+            r#"[{
+            "edge": {
+                "child": "0xfaa",
+                "path": {
+                    "len": 1,
+                    "value": "0xbad"
+                }
+            }
+        }]"#,
+        )
+        .unwrap();
+        assert!(StorageProof::parse_proof(&key, value, &mut proof).is_none());
+    }
+
+    #[test]
+    fn invalid_one_level_proof_last_key_byte_2_instead_of_1() {
+        let key = felt_to_bits(
+            FieldElement::from_str(
+                "0x0341c1bdfd89f69748aa00b5742b03adbffd79b8e80cab5c50d91cd8c2a79be2",
+            )
+            .unwrap(),
+        );
+        let value = FieldElement::from_str(
+            "0x000000000000000000000000000047616d65206f66204c69666520546f6b656e",
+        )
+        .unwrap();
+        let edge_node_string = r#"[{
+            "edge": {
+                "child": "0x47616d65206f66204c69666520546f6b656e",
+                "path": {
+                    "len": 231,
+                    "value": "0x3dfd89f69748aa00b5742b03adbffd79b8e80cab5c50d91cd8c2a79be1"
+                }
+            }
+        }]"#;
+        let mut proof: Vec<TrieNode> =
+            serde_json::from_str(edge_node_string).unwrap();
+        assert!(StorageProof::parse_proof(&key, value, &mut proof).is_none());
+    }
+
+    #[test]
+    fn invalid_five_level_proof_len_7_instead_of_1() {
+        let key = felt_to_bits(
+            FieldElement::from_str(
+                "0x0341c1bdfd89f69748aa00b5742b03adbffd79b8e80cab5c50d91cd8c2a79be1",
+            )
+            .unwrap(),
+        );
+        let value = FieldElement::from_str(
+            "0x000000000000000000000000000047616d65206f66204c69666520546f6b656e",
+        )
+        .unwrap();
+        let proof_string = r#"[
+        {
+            "binary": {
+                "left": "0x46e82293b0564764a071f1aa4488aa7577b1b5bb2e898321f8536d5593d371d",
+                "right": "0x58adcf6ea8b96992aa316e2f092f2480ca406c3630fe97573046a32900745b5"
+            }
+        },
+        {
+            "binary": {
+                "left": "0x716e211c75f4c0e14dbe46c361812b0129abd061b63faf91ad5569bf22b785c",
+                "right": "0x3729d9699d4410223e413f3b3aa91a043d94242f888188036e6ea25b6962041"
+            }
+        },
+        {
+            "edge": {
+                "child": "0x6281e42b5941ae1a77ea03836aad1190097f72e1a1ed534fae2e00b4118f504",
+                "path": {
+                    "len": 7,
+                    "value": "0x1"
+                }
+            }
+        },
+        {
+            "binary": {
+                "left": "0x3e3800516f62800ef6491b1cb1915b3353026ea6a6afcf35e8d4c54e35b04ea",
+                "right": "0x1e224db31dfb3e1b8c95670a12f1903d4a32ac7bb83f4b209029e14155bbca9"
+            }
+        },
+        {
+            "edge": {
+                "child": "0x47616d65206f66204c69666520546f6b656e",
+                "path": {
+                    "len": 231,
+                    "value": "0x3dfd89f69748aa00b5742b03adbffd79b8e80cab5c50d91cd8c2a79be1"
+                }
+            }
+        }]"#;
+        let mut proof: Vec<TrieNode> =
+            serde_json::from_str(proof_string).unwrap();
+        assert!(StorageProof::parse_proof(&key, value, &mut proof).is_none());
+    }
+
+    #[test]
+    fn valid_one_level_verify_proof() {
+        let mut storage_proof = StorageProof {
+            contract_data: ContractData {
+                storage_proofs: vec![serde_json::from_str(
+                    r#"[{
+                    "edge": {
+                        "child": "0x47616d65206f66204c69666520546f6b656e",
+                        "path": {
+                            "len": 231,
+                            "value": "0x3dfd89f69748aa00b5742b03adbffd79b8e80cab5c50d91cd8c2a79be1"
+                        }
+                    }
+                }]"#,
+                )
+                .unwrap()],
+                root: FieldElement::from_hex_be(
+                    "0x1e224db31dfb3e1b8c95670a12f1903d4a32ac7bb83f4b209029e14155bbca9",
+                )
+                .unwrap(),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let key = felt_to_bits(
+            FieldElement::from_str(
+                "0x0341c1bdfd89f69748aa00b5742b03adbffd79b8e80cab5c50d91cd8c2a79be1",
+            )
+            .unwrap(),
+        );
+        let value = FieldElement::from_str(
+            "0x000000000000000000000000000047616d65206f66204c69666520546f6b656e",
+        )
+        .unwrap();
+
+        assert!(storage_proof.verify_storage_proofs(&key, value).is_ok());
+    }
+
+    #[test]
+    fn invalid_one_level_verify_proof() {
+        let mut storage_proof = StorageProof {
+            contract_data: ContractData {
+                storage_proofs: vec![serde_json::from_str(
+                    r#"[{
+                    "edge": {
+                        "child": "0xbad",
+                        "path": {
+                            "len": 231,
+                            "value": "0xfaa"
+                        }
+                    }
+                }]"#,
+                )
+                .unwrap()],
+                root: FieldElement::from_dec_str("42").unwrap(),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let key = felt_to_bits(FieldElement::default());
+        let value = FieldElement::ONE;
+
+        assert!(storage_proof.verify_storage_proofs(&key, value).is_err());
+    }
+
+    #[test]
+    fn contract_state_hash_is_valid() {
+        let storage_proof = StorageProof {
+            contract_data: ContractData {
+                class_hash: FieldElement::ONE,
+                root: FieldElement::TWO,
+                nonce: FieldElement::THREE,
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+        assert_eq!(
+            storage_proof.calculate_contract_state_hash(),
+            FieldElement::from_hex_be(
+                "0x7fdeb85518534a06e6b50c2ccdea7bbf3d47c607a9b36fbf690c41274976950"
+            )
+            .unwrap()
+        );
+    }
+
+    #[test]
+    fn calculate_global_root_is_valid() {
+        let storage_proof = StorageProof {
+            class_commitment: FieldElement::ONE,
+            ..Default::default()
+        };
+        assert_eq!(
+            storage_proof.calculate_global_root(FieldElement::TWO),
+            FieldElement::from_hex_be(
+                "0x57e17712cba54d27d07c13c60861ccb7aa16fa4cfad71845d63a4448203953c"
+            )
+            .unwrap()
+        );
+    }
+
+    #[test]
+    fn valid_verify_contract_proof() {
+        let mut storage_proof = StorageProof {
+            contract_proof: serde_json::from_str(
+                r#"[{
+                "edge": {
+                    "child": "0x538a7653ef22e217f93066ac54784c0159a5e1e37d808f83c82d1b42d57457d",
+                    "path": {
+                        "len": 229,
+                        "value": "0x4a03bb9e744479e3298f54705a35966ab04140d3d8dd797c1f6dc49d0"
+                    }
+                }
+            }]"#,
+            )
+            .unwrap(),
+            state_commitment: FieldElement::from_hex_be(
+                "0x1e2a7a7ee40c1d897c8c0a9515720ea02c8075ee9e00db277f5f8c3e4edcb54",
+            )
+            .unwrap(),
+            contract_data: ContractData {
+                class_hash: FieldElement::from_hex_be(
+                    "0x4e635d495504b31ec191cbfc3d99b5d109bfcae4d0d9e16f4909a43b2e24c07",
+                )
+                .unwrap(),
+                root: FieldElement::from_hex_be(
+                    "0x5826149cbab3f8538d346301869ba2742a159d1542463ce19a60a927b826a2f",
+                )
+                .unwrap(),
+                nonce: FieldElement::ZERO,
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+        let global_root = storage_proof.state_commitment;
+        let contract_address = FieldElement::from_hex_be(
+            "0x06a05844a03bb9e744479e3298f54705a35966ab04140d3d8dd797c1f6dc49d0",
+        )
+        .unwrap();
+        assert!(storage_proof
+            .verify_contract_proof(global_root, contract_address)
+            .is_ok());
+    }
+
+    #[test]
+    fn invalid_verify_contract_proof() {
+        let mut invalid_storage_proof = StorageProof::default();
+        let global_root = FieldElement::ZERO;
+        let contract_address = FieldElement::ZERO;
+        assert!(invalid_storage_proof
+            .verify_contract_proof(global_root, contract_address)
+            .is_err());
+    }
+}
