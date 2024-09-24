1535cb4252335cfbf10ff3f4c0e4a2bec7b761d7
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-09-24 14:59:12 +0200

    feat(web): add simple web UI

--- web/app.js
+++ web/app.js
@@ -1,36 +1,23 @@
+var ready;
+
 const worker = new Worker(new URL('./wrk.js', import.meta.url), { type: 'module' });
 worker.onmessage = event => {
     if (!ready) {
         if (event.data === 'OK') {
-            dump('log', 'Worker ready');
+            dump('log', 'worker ready');
             ready = true;
-
-            setTimeout(() => {
-                post('{"state": {}}');
-            }, 1000);
-            
-            setTimeout(() => {
-                post(`{"execute": ${request}}`);
-            }, 10000);
+            set_status(event.data);
         } else {
             dump('log', event.data, 'error');
+            set_status(event.data);
         }
         return;
     }
-    dump('log', event.data);
+    dump('log', '<<< ' + event.data);
 };
 worker.onerror = error => {
     dump('log', error, 'error');
 }
-
-var ready;
-
-const config = JSON.stringify({
-    network: "mainnet",
-    ethereum_url: "http://127.0.0.1:3000/eth-mainnet.g.alchemy.com/v2/nLWc0kd1-CBFNJ57gvB_lVcQpSYCGZS1",
-    starknet_url: "http://127.0.0.1:3000/starknet-mainnet.g.alchemy.com/v2/nLWc0kd1-CBFNJ57gvB_lVcQpSYCGZS1"
-});
-worker.postMessage(config);
 
 const request = JSON.stringify({
     "calldata": [],
@@ -42,7 +29,7 @@ function post(message) {
     if (!ready) {
         throw new Error('worker not ready');
     }
-    dump('log', message);
+    dump('log', '>>> ' + message);
     worker.postMessage(message);
 }
 
@@ -60,3 +47,60 @@ function dump(id, text, style) {
     p.innerText = text;
     div.appendChild(p);
 }
+
+function get(id) {
+    return document.getElementById(id);
+} 
+
+function run() {
+    var key = get('key');
+    var setup = get('setup');
+    setup.onclick = () => {
+        const config = JSON.stringify({
+            network: "mainnet",
+            ethereum_url: `http://127.0.0.1:3000/eth-mainnet.g.alchemy.com/v2/${key.value}`,
+            starknet_url: `http://127.0.0.1:3000/starknet-mainnet.g.alchemy.com/v2/${key.value}`
+        });
+        worker.postMessage(config);
+        set_status('wait');
+    }
+
+    var state = get('get');
+    state.disabled = true;
+    state.onclick = () => {
+        post('{"state": {}}');
+    };
+
+    var exe = get('exe');
+    exe.disabled = true;
+    exe.onclick = () => {
+        post(`{"execute": ${request}}`);
+    };
+}
+
+function set_status(message) {
+    var status = get('status');
+    if (message === 'OK') {
+        status.innerText = 'READY';
+        status.classList.remove('status-wait');
+        status.classList.add('status-ready');
+        get('setup').disabled = true;
+        get('get').disabled = false;
+        get('exe').disabled = false;
+    } else if (message === 'wait') {
+        status.innerText = 'WAIT';
+        status.classList.add('status-wait');
+        get('setup').disabled = true;
+        get('get').disabled = true;
+        get('exe').disabled = true;
+    } else {
+        status.innerText = 'ERROR';
+        status.classList.remove('status-wait');
+        status.classList.add('status-error');
+        get('setup').disabled = false;
+        get('get').disabled = true;
+        get('exe').disabled = true;
+    }
+}
+
+run();
