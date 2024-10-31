8a482776156b51abcd72ae0548770df7d40626ee
Sergey Melnychuk <sergey-melnychuk@users.noreply.github.com>
2024-10-31 23:09:12 +0100

    feat(wasm): store recent L1 state

--- web/beerus-web/src/lib.rs
+++ web/beerus-web/src/lib.rs
@@ -152,6 +152,7 @@ pub fn set_panic_hook() {
 #[wasm_bindgen]
 pub struct Beerus {
     beerus: beerus::client::Client<Http>,
+    state: Option<beerus::client::State>,
 }
 
 #[wasm_bindgen]
@@ -167,37 +168,42 @@ impl Beerus {
             ethereum_rpc: config.ethereum_url,
             starknet_rpc: config.starknet_url,
         };
-        web_sys::console::log_1(&"beerus: config ready".into());
         let beerus = beerus::client::Client::new(&config, Http(Rc::new(f)))
             .await
             .map_err(|e| JsValue::from_str(&format!("failed to create client: {e:?}")))?;
-        web_sys::console::log_1(&"beerus: client ready".into());
-        Ok(Self { beerus })
+        web_sys::console::log_1(&"beerus: ready".into());
+        Ok(Self { beerus, state: None })
     }
 
     #[wasm_bindgen]
-    pub async fn get_state(&self) -> Result<JsValue, JsValue> {
+    pub async fn get_state(&mut self) -> Result<JsValue, JsValue> {
         let state = self.beerus.get_state().await
             .map_err(|e| JsValue::from_str(&format!("failed to get state: {e:?}")))?;
-        let state = serde_json::to_string(&dto::State {
+
+        let ret = serde_json::to_string(&dto::State {
             len: state.block_number as i64,
             hash: state.block_hash.as_ref().to_owned(),
             root: state.root.as_ref().to_owned(),
         }).map_err(|e| JsValue::from_str(&format!("failed to serialize state: {e:?}")))?;
-        Ok(JsValue::from_str(&state))
+        let ret = JsValue::from_str(&ret);
+
+        self.state = Some(state);
+        Ok(ret)
     }
 
     #[wasm_bindgen]
-    pub async fn execute(&self, request: &str) -> Result<JsValue, JsValue> {
-        let state = self.beerus.get_state().await
-            .map_err(|e| JsValue::from_str(&format!("failed to get state: {e:?}")))?;
-        web_sys::console::log_1(&"beerus: execute: state ready".into());
+    pub async fn execute(&mut self, request: &str) -> Result<JsValue, JsValue> {
+        if self.state.is_none() {
+            let _ = self.get_state().await?;
+        }
+        let state = self.state.clone().unwrap();
+
         let request: beerus::gen::FunctionCall = serde_json::from_str(request)
             .map_err(|e| JsValue::from_str(&format!("failed to parse request: {e:?}")))?;
-        web_sys::console::log_1(&"beerus: execute: request ready".into());
+
         let result = self.beerus.execute(request, state)
             .map_err(|e| JsValue::from_str(&format!("failed to execute call: {e:?}")))?;
-        web_sys::console::log_1(&format!("beerus: execute: {result:?}").into());
+
         let result = serde_json::to_string(&result)
             .map_err(|e| JsValue::from_str(&format!("failed to serialize call result: {e:?}")))?;
         Ok(JsValue::from_str(&result))
